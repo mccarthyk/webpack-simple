8128c35f1dc38986e1f091fc5170f2d49b9da04f
Kevin McCarthy <mccarthyk@hcflgov.net>
2018-07-25 14:42:58 -0400

    init VueRouter & Vuex scaffolding

---
+++ template/src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// import store from '../store'
+// import Hello from '../components/Hello'
+
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    // {
+    //   path: '/',
+    //   component: Hello
+    // },
+    // {
+    //   path: '*',
+    //   component: Hello
+    // }
+  ]
+})
+
+export default router
